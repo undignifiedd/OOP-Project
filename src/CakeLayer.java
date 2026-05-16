9d97309cae0b89329d273dec81e95588625fe5cd
Yusra <y53637624@gmail.com>
2026-05-16 10:56:58 +0500

    Initial commit

---
+++ src/CakeLayer.java
@@ -0,0 +1,31 @@
+public class CakeLayer {
+    private String layerType;
+    private Ingredient ingredient;
+    private boolean isApplied;
+    public CakeLayer (String layerType){
+        this.layerType=layerType;
+        this.ingredient=null;
+        this.isApplied=false;
+    }
+    public void apply(Ingredient ingredient){
+        this.ingredient=ingredient;
+        isApplied=true;
+    }
+    public boolean isCorrect(CakeLayer other){
+        if (this.ingredient.getName().equals(other.ingredient.getName())){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public String getLayerType() {
+        return layerType;
+    }
+    public boolean isApplied() {
+        return isApplied;
+    }
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+}
