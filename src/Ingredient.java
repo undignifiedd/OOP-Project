58dfcab3caf60349320efb0a5aeba1258be0f85e
Yusra <y53637624@gmail.com>
2026-05-16 10:34:24 +0500

    Add Ingredient class with fields and constructor

--- src/Ingredient.java
+++ src/Ingredient.java
@@ -1,3 +1,13 @@
 public class Ingredient {
-
+    private String name, type;
+    public Ingredient(String name, String type){
+        this.name=name;
+        this.type=type;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getType() {
+        return type;
+    }
 }
