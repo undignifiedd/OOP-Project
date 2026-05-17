06e9ed532204c7f2f2ef52bdddc278e133c72552
muntaha <muntahatoqeer585@gmail.com>
2026-05-17 14:31:03 +0500

    Adding sprites to the player

--- src/Entity.java
+++ src/Entity.java
@@ -1,5 +1,11 @@
+import java.awt.image.BufferedImage;
+
 public class Entity {
     int x;
     int y;
     int speed;
+
+    public BufferedImage chef_right, chef_kick_right, chef_left, chef_kick_left, chef_up;
+
+    public String direction;
 }
--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,9 @@
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Player extends Entity {
     GamePanel gamePanel;
     KeyHandler keyHandler;
@@ -5,17 +11,67 @@ public class Player extends Entity {
     public Player(GamePanel gamePanel, KeyHandler keyHandler) {
         this.gamePanel = gamePanel;
         this.keyHandler = keyHandler;
+
+        setDefaultValues();
+        getPlayerImage();
+    }
+
+    public void getPlayerImage() {
+
+        try {
+            chef_right = ImageIO.read(getClass().getResourceAsStream("/player/cheffaceright.png"));
+            chef_kick_right = ImageIO.read(getClass().getResourceAsStream("/player/chefkickright.png"));
+            chef_left = ImageIO.read(getClass().getResourceAsStream("/player/cheffaceleft.png"));
+            chef_kick_left = ImageIO.read(getClass().getResourceAsStream("/player/chefkickleft.png"));
+            chef_up = ImageIO.read(getClass().getResourceAsStream("/player/chefback.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "right";
     }
 
     public void update() {
+        if (keyHandler.upKeyPressed == true) {
+            direction = "up";
+            y -= speed;
+        } else if (keyHandler.downKeyPressed == true) {
+            direction = "down";
+            y += speed;
+        } else if (keyHandler.leftKeyPressed == true) {
+            direction = "left";
+            x -= speed;
+        } else if (keyHandler.rightKeyPressed == true) {
+            direction = "right";
+            x += speed;
+        }
     }
 
-    public void draw() {
+    public void draw(Graphics2D g2) {
+        BufferedImage image = null;
+        switch (direction) {
+            case "up":
+                image = chef_up;
+                break;
+            case "down":
+                image = chef_right;
+                break;
+            case "left":
+                image = chef_left;
+                break;
+            case "right":
+                image = chef_right;
+                break;
+            default:
+                break;
+        }
+        if (image != null) {
+            g2.drawImage(image, x, y, null);
+        }
     }
 }
